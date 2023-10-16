d193642f7ee6b707756657f83cb79c8f75deeb9c
XOMRKOB <ru89050995432@yandex.ru>
2023-10-16 09:33:20 +0600

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import ToggleSwitch from "./App/components/ToggleSwitch";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <ToggleSwitch />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/App/components/ToggleSwitch.tsx
@@ -0,0 +1,41 @@
+import { motion } from 'framer-motion'
+import {useState} from "react";
+
+const ToggleSwitch = () => {
+    const [isOn, setIsOn] = useState<boolean>(false)
+    const toggleSwitch = () => setIsOn(!isOn)
+
+    return (
+        <>
+            <div
+                style={{
+                    background: isOn ? "#48bb78" : "rgba(203,213,224,0.5)",
+                    justifyContent: isOn ? "flex-end" : 'flex-start',
+                    width: "6rem",
+                    padding: "0.25rem",
+                    display: "flex",
+                    borderRadius: 9999,
+                    cursor: "pointer"
+                }}
+                onClick={toggleSwitch}
+            >
+                <motion.div
+                    style={{
+                        width: "3rem",
+                        height: "3rem",
+                        background: "white",
+                        borderRadius: "100%",
+                        boxShadow:
+                            "0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)",
+                    }}
+                    layout
+                ></motion.div>
+            </div>
+            <div>
+                {isOn.toString()}
+            </div>
+        </>
+    )
+}
+
+export default ToggleSwitch
